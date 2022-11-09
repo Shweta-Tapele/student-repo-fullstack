9ed7254497b882cbe57af580b6f9f09fd54845ef
Stapele93 <91915044+Stapele93@users.noreply.github.com>
2022-11-08 20:15:33 -0800

    formatted

--- hw3/03-form.js
+++ hw3/03-form.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5001;
 
 // http://localhost:5001/submit should return all the data the user entered
 const server = http.createServer((req, res) => {
-const formHTML = `<form action="/submit" method="post" id="submission">
+const formpage = `<form action="/submit" method="post" id="submission">
 Username: <input name="username id="username"><br>
 Email: <input name="email" id="email"><br>
 Comments: <input name="comments" id=comments><br>
@@ -14,23 +14,28 @@ Newsletter: <input name="newsletter" type="checkbox" id="accept">
 </form>`; 
 
 if(req.url === '/form'){
-  //lastPost = "";
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.write(formHTML);
+  res.write(formpage);
   res.end();
 }
 else if(req.url === '/submit'){
   res.writeHead(200, { "Content-Type": "text/html" });
-  username = formHTML.match("username");
-        email = formHTML.match("email");
-        comments = formHTML.match("comments");
-        newsletter = formHTML.match("newsletter");
+  username = formpage.match("username");
+        email = formpage.match("email");
+        comments = formpage.match("comments");
+        newsletter = formpage.match("newsletter");
         res.write('<h1> Submission Results: <h1>');
-        res.write(`<h2> Username: ${username} <h2>`);
-        res.write(`<h2> Email: ${email} <h2>`);
-        res.write(`<h2> Comments: ${comments} <h2>`);
-        if(newsletter != false){res.write(`<h2> Newsletter: Yes,sign me up for the news letter.<h2>`);}
-        else{res.write('<h2> Newsletter: No, Thank you<h2>');}
+        res.write(`<h3> Username: ${username} <h2>`);
+        res.write(`<h3> Email: ${email} <h2>`);
+        res.write(`<h3> Comments: ${comments} <h2>`);
+        if(newsletter != false)
+        {
+          res.write(`<h2> Newsletter: Yes,sign me up for the news letter.<h2>`);
+        }
+        else
+        {
+          res.write('<h2> Newsletter: No, Thank you<h2>');
+        }
         res.end();
         }
       });
