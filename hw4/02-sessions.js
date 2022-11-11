5000c2b83eaeb9c58d12c5b0acd7b3d1f48e836d
Stapele93 <91915044+Stapele93@users.noreply.github.com>
2022-11-11 00:11:56 -0800

    session update for favicon

--- hw4/02-sessions.js
+++ hw4/02-sessions.js
@@ -6,10 +6,27 @@ const port = process.env.PORT || 5001;
 // Add your code here
 
 // Use the express-session module
-app.use(/** ... */);
+app.use(
+  session({
+  store: new session.MemoryStore(),
+  secret: 'a secret to sign the cookie',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 86400000,
+  },
+})
+);
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   // Add your code here
+  res.status(200);
+  res.set({'Content-Type': 'text/plain'});
+  if (req.session.visit === undefined){
+    req.session.visit = [];
+    req.session.visit.push('/');
+    res.write('Current route: ')
+  }
 });
 
 app.listen(port, () => {
